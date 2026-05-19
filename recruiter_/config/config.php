0e5b1b3773d675329dd7049dc83f42cfc694ce5f
Khaled Raihan <ctgworld420@gmail.com>
2026-05-19 07:06:09 +0600

    Delete recruiter_ directory

--- recruiter_/config/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-$conn = mysqli_connect("localhost", "root", "", "jobportal_db");
-if (!$conn) {
-    die("Connection failed: " . mysqli_connect_error());
-}
-mysqli_set_charset($conn, "utf8");
-?>
